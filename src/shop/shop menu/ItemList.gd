452765709802b56ba20e51f6e96a5af1a437b105
salvage lucas <lucas.salvage@epitech.eu>
2024-03-23 20:37:02 +0100

    [ADD] : forge to shop

--- src/shop/shop menu/ItemList.gd
+++ src/shop/shop menu/ItemList.gd
@@ -3,7 +3,7 @@ extends ItemList
 var indexes
 
 func _ready():
-	indexes = ["res://shop/bitcoin page/bitcoin_shop.tscn"]
+	indexes = ["res://shop/bitcoin page/bitcoin_shop.tscn", "res://kyou/kyou_vert/minerai_vert.tscn"]
 
 func _on_item_activated(index):
 	get_tree().change_scene_to_file(indexes[index])
