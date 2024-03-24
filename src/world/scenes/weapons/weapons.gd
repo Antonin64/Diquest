0e7c13865c719a9be4a415bfc7bb6fda5c8b0771
CB4fr2 <baptiste.camerlynck@epitech.eu>
2024-03-24 05:30:22 +0100

    Added inventory

--- src/world/scenes/weapons/weapons.gd
+++ src/world/scenes/weapons/weapons.gd
@@ -1,23 +1,13 @@
 extends Node2D
 
-@onready var dmg = 100
-@onready var atk_speed = 1
-@onready var type = "sword"
 @onready var modifier = $Modifier
+var sword_damage = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func player_attack(vector_dir : Vector2):
-	if (type == "sword"):
-		$attack_orientation.visible = true
-		$attack_orientation.rotation = vector_dir.angle()
-		$attack_orientation/attachment/atk_anim.play("sword_attack", -1, 1, false)
+func player_attack(vector_dir : Vector2, damage : int):
+	sword_damage = damage
+	$attack_orientation.visible = true
+	$attack_orientation.rotation = vector_dir.angle()
+	$attack_orientation/attachment/atk_anim.play("sword_attack", -1, 1, false)
 
 func hide_sword():
 	$attack_orientation.visible = false
@@ -25,25 +15,4 @@ func hide_sword():
 func _is_sword_hitting():
 	var arr = $attack_orientation/Area2D.get_overlapping_bodies()
 	for i in arr:
-		i.take_damage(dmg)
-
-func set_sword(sword):
-	type = "sword"
-	if (sword == "sword_common"):
-		$attachment/Sprite2D.set_texture("res://world/assets/weapons/swords/sword_common.png")
-		modifier.Stats["damage"] = 1
-	if (sword == "sword_uncommon"):
-		$attachment/Sprite2D.set_texture("res://world/assets/weapons/swords/sword_uncommon.png")
-		modifier.Stats["damage"] = 3
-	if (sword == "sword_rare"):
-		$attachment/Sprite2D.set_texture("res://world/assets/weapons/swords/sword_rare.png")
-		modifier.Stats["damage"] = 6
-	if (sword == "sword_epic"):
-		$attachment/Sprite2D.set_texture("res://world/assets/weapons/swords/sword_epic.png")
-		modifier.Stats["damage"] = 12
-	if (sword == "sword_legendary"):
-		$attachment/Sprite2D.set_texture("res://world/assets/weapons/swords/sword_legendary.png")
-		modifier.Stats["damage"] = 20
-	if (sword == "sword_mythical"):
-		$attachment/Sprite2D.set_texture("res://world/assets/weapons/swords/sword_mythical.png")
-		modifier.Stats["damage"] = 30
+		i.take_damage(sword_damage)
