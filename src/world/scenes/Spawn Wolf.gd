332e14d265159d77bfd576012f05fc6a025985dc
Gab-Lrg <gabriel.large@epitech.eu>
2024-03-24 06:11:26 +0100

    ADD: yo

---
+++ src/world/scenes/Spawn Wolf.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var mob
+
+func _ready():
+	mob = load("res://Enemies/Loup/Loup.tscn").instantiate()
+	mob.player_node = $"../CharacterBody2D"
+	add_child(mob)
+
+func _process(_delta):
+	if mob == null and $Timer.is_stopped():
+		$Timer.start()
+
+func _on_timer_timeout():
+	mob = load("res://Enemies/Loup/Loup.tscn").instantiate()
+	mob.player_node = $"../CharacterBody2D"
+	add_child(mob)
